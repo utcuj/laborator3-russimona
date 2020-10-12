35593cb478b0d6bfd698145dd2724b477a00227e
cristinaradulescu <cristina.radulescu@metrosystems.net>
2020-10-12 12:53:45 +0300

    Modified.

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
 
     public static void main( String[] args ) {
 
-        System.out.println( "Modify this message" );
+        System.out.println( "Modify this message!" );
 
     }
 }
